328ff4e00a6aae01dcefc7700b4ec9443cc261b6
Алексей Назаренко <Garenekef1@users.noreply.github.com>
2026-04-18 19:22:54 +0300

    Destructuring in JS has been studied

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("Lab 24 start");
+console.log("Step 6 destructuring");
+
+const product = {
+  name: "Ноутбук",
+  price: 79990,
+  category: "Электроника",
+  inStock: true
+};
+
+const { name, price, category, inStock } = product;
+console.log(name, price, category, inStock);
+
+function printProduct({ name, price, category, inStock }) {
+  console.log("Товар:", name, "| Цена:", price, "| Категория:", category, "| В наличии:", inStock);
+}
+
+printProduct(product);

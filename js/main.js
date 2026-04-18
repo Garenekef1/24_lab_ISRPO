199740c88103de147b7f4ec7229fd8f731e62ebf
Алексей Назаренко <Garenekef1@users.noreply.github.com>
2026-04-18 19:23:25 +0300

    spread/rest in JS

--- js/main.js
+++ js/main.js
@@ -15,3 +15,20 @@ function printProduct({ name, price, category, inStock }) {
 }
 
 printProduct(product);
+
+console.log("Step 7 spread/rest");
+
+const numbersA = [2, 4, 6];
+const numbersB = [1, 3, 5];
+const mergedNumbers = [...numbersA, ...numbersB];
+console.log(mergedNumbers);
+
+function findMax(...numbers) {
+  return Math.max(...numbers);
+}
+console.log(findMax(...mergedNumbers));
+
+const profileBase = { name: "Alexey", age: 19 };
+const profileExtra = { city: "Tomsk", hobby: "coding" };
+const fullProfile = { ...profileBase, ...profileExtra };
+console.log(fullProfile);
